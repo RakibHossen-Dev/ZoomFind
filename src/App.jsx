3aad7ca58b04e3e98be9a969c0c587d57e4ceb31
Rakib <hellorakibhossen@gmail.com>
2025-04-08 20:19:34 +0600

    Environment setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="text-2xl font-bold bg-green-500 p-5">ZoomFind</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div>This is navber</div>;
+};
+
+export default Navbar;
